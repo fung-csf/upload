53545ed4e5b44a41c5f894cf3966f60ae959b8ca
eshan25 <eshankang@gmail.com>
2021-08-28 12:13:11 +0800

    Added confirmation box

--- src/UploadAlt.tsx
+++ src/UploadAlt.tsx
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
-
 import axios from "axios";
-
-import {
- 
-  Card,
-  Button,
-  Modal,
-  
-} from "antd";
-
+import { Card, Button, Modal } from "antd";
 import FileInput from './FileInput.tsx'
 
 
 function UploadAlt() {
 
     const [fileData, setfileData] = useState();
-    
+    const [isModalVisible, setIsModalVisible] = useState(false);    
     const [fileDataContent, setfileDataContent] = useState(" ");
 
     function fileContent(file) {
@@ -25,9 +16,41 @@ function UploadAlt() {
         // console.log(file[0]);
       }
 
-    
+      const showModal = () => {
+        setIsModalVisible(true);
+      };
 
-    return (
+      const handleCancel = () => {
+        setIsModalVisible(false);
+      };
+
+      const handleSubmit = (e) => {
+        var fr = new FileReader();
+        fr.onload = function (e) {
+          // e.target.result should contain the text
+          var text = e.target.result;
+          //encode string to base64
+          setfileDataContent(text);
+          console.log(fileDataContent);
+        };
+        fr.readAsText(fileData);
+        axios({
+          method: "post",
+          url: "",
+          //database table with the columns Name, content
+          data: [{ Name: fileData.name, content: fileDataContent }]
+        })
+          .then(function (response) {
+            // handle success
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          });
+        };
+
+      return (
         <div>
         <Card
             style={{
@@ -35,14 +58,28 @@ function UploadAlt() {
             width: 386,
             paddingBottom: "20px",
             marginBottom: "20px"
-            }}
-        >
+            }}>
             <FileInput fileContent={fileContent} />
-
-        
+            <Button type="primary" onClick={showModal} style={{marginTop: "50px"}}>
+            Upload
+            </Button>
+            <Modal
+            title="Confirmation"
+            visible={isModalVisible}
+            onOk={handleSubmit}
+            onCancel={handleCancel}>
+            <p>Are you sure to upload the following file?</p>
+            {fileData && <p>File: {fileData.name}</p>}
+            <p
+                style={{
+                paddingTop: "8px",
+                fontFamily: "Arial",
+                fontWeight: "bold"
+                }}>
+            </p>
+            </Modal>
         </Card>
     </div>
     );
   }
-
   export default UploadAlt;
